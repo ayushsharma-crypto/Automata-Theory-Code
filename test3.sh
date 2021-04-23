404fe2c7c800e25cd0d7425b091bd88b0beea7e5
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-04-23 16:01:15 +0530

    q3 complete

--- test3.sh
+++ test3.sh
@@ -1,3 +1,4 @@
 python q3.py ./q3-test-cases/0.txt ./q3-output/0.txt
 python q3.py ./q3-test-cases/1.txt ./q3-output/1.txt
 python q3.py ./q3-test-cases/2.txt ./q3-output/2.txt
+python q3.py ./q3-test-cases/3.txt ./q3-output/3.txt
